dc524dd839745ae2e5fe6baf3b93760205387ef0
DanielHe09 <dandanhe09@gmail.com>
2025-07-01 21:48:54 -0400

    initial commit, added a Bootstrap list-group

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import ListGroup from "./components/ListGroup"
+
+function App() {
+  return <div><ListGroup /></div>;
+}
+
+//makes the App component accessible to other files in our project
+export default App;
