cb9df45ad9ead6d68ca6ffcfb1e2ca68da3e79fe
DanielHe09 <dandanhe09@gmail.com>
2025-07-01 23:06:28 -0400

    rendering lists

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,12 +1,25 @@
+
 function ListGroup(){
+    const items = [
+        'New York',
+        'San Francisco',
+        'Tokyo',
+        'London',
+        'Paris'
+    ]
+
     return (
-        <ul className="list-group">
-            <li className="list-group-item">An item</li>
-            <li className="list-group-item">A second item</li>
-            <li className="list-group-item">A third item</li>
-            <li className="list-group-item">A fourth item</li>
-            <li className="list-group-item">And a fifth one</li>
-        </ul>
+        //if we use empty brackets, we are telling React to wrap the multiple elements into a fragment
+        <>
+            <h1>List</h1>
+            <ul className="list-group">
+                <li className="list-group-item">An item</li>
+                <li className="list-group-item">A second item</li>
+                <li className="list-group-item">A third item</li>
+                <li className="list-group-item">A fourth item</li>
+                <li className="list-group-item">And a fifth one</li>
+            </ul>
+        </>
     );
 }
 
