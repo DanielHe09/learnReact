2cc3b407e72f8ae8a9c9241a23d977ffa9097ddb
DanielHe09 <dandanhe09@gmail.com>
2025-07-10 20:56:11 -0400

    added conditional rendering

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,5 +1,5 @@
 function ListGroup(){
-    const items = [
+    let items = [
         'New York',
         'San Francisco',
         'Tokyo',
@@ -7,14 +7,20 @@ function ListGroup(){
         'Paris'
     ]
 
-    
+    items=[]    
 
+    //in return statements you can only use html or other react components unless inside brackets
     return (
         //if we use empty brackets, we are telling React to wrap the multiple elements into a fragment
         <>
             <h1>List</h1>
+            {
+            //if the condition is true we return No item found else nothing happens
+            items.length === 0 && <p>No item found</p>
+            }
             <ul className="list-group">
                 {items.map((item) => (
+                    //when using the map function, we have to give every item a key, in this case we can just use the item itself since it's a string
                     <li key={item}>{item}</li>
                 ))}
             </ul>
